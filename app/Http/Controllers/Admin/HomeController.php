e429fa4400834182dd94818adf483587f5291954
Frinaldo Sinaga <84637791+Sinagafrinaldo@users.noreply.github.com>
2022-10-30 21:34:58 +0700

    update controller user dan admin

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
-
+ 
 class HomeController extends Controller
 {
     public function __construct()
@@ -15,23 +15,29 @@ class HomeController extends Controller
     }
 
     public function index (){
+        $jumlah = DB::table('barang')->count();
+        $kategori = DB::table('kategori')->count();
         return view('dashboard.home' , [
-            "title" => "dashboard"
+            "title" => "dashboard",
+            "jumlah"=>$jumlah,
+            "kategori"=>$kategori
         ]); 
     }
     public function inventory (){
         return view('dashboard.inventory', [
             "title" => "inventory"
-        ]); 
+        ]);  
     }
     public function daftarObat (){
         $barang = DB::table('barang')
 		->orderby('id' , 'desc')
 		->paginate(10)->onEachSide(0);
 
+        $kategori = DB::table('kategori')->get();
         return view('dashboard.inventori.daftarObat', [
             "title" => "Daftar Obat",
-            "barang" => $barang
+            "barang" => $barang,
+            "kategori"=>$kategori
         ]); 
     }
     public function inputTransaksi (){
@@ -40,8 +46,11 @@ class HomeController extends Controller
         ]); 
     }
     public function inputObat (){
+        $kategori = DB::table('kategori')->get();
+
         return view('dashboard.inventori.tambahObat', [
-            "title" => "Input Obat"
+            "title" => "Input Obat",
+            "kategori" => $kategori
         ]); 
     }
 
@@ -73,8 +82,13 @@ class HomeController extends Controller
     }
 
     public function kategori (){
+        $kategori = DB::table('kategori')
+		->orderby('id' , 'desc')
+		->paginate(8)->onEachSide(0);
+
         return view('dashboard.kategori.home',[
-            "title" => "Kategori"
+            "title" => "Kategori",
+            "kategori" => $kategori
         ]); 
     }
 
@@ -84,10 +98,12 @@ class HomeController extends Controller
         ]); 
     }
 
-    public function editKategori (){
-        return view('dashboard.kategori.edit',[
+    public function editKategori (Request $request){
+        $kategori = DB::table('kategori')->where('slug',$request->slug)->get();
+		return view('dashboard.kategori.edit',[
+            'kategori' => $kategori,
             "title" => "Edit Kategori"
-        ]); 
+        ]);
     }
 
 
@@ -101,7 +117,8 @@ class HomeController extends Controller
             'kadaluarsa'=> 'required',
             'deskripsi'=> 'required',
             'kategori'=> 'required',
-			'image' => 'required|mimes:jpg,jpeg,png,bmp,gif,svg'
+			'image' => 'required|mimes:jpg,jpeg,png,bmp,gif,svg',
+            'harga' =>'required'
 
 		]);
 		$img = $request->file('image');
@@ -119,9 +136,9 @@ class HomeController extends Controller
             'minimal' =>$request->minimal,
             'maksimal' =>$request->maksimal,
             'kadaluarsa' =>$request->kadaluarsa,
-            'deskripsi' =>$request->deskripsi,
 			'image' =>$name,
-			'slug' =>$slug
+			'slug' =>$slug,
+            'harga' =>$request->harga,
 		]);
 		return redirect('/admin/daftar-obat');
 	
@@ -137,12 +154,177 @@ class HomeController extends Controller
 
     public function edit(Request $request)
 	{
+    $kategori = DB::table('kategori')->get();
 	$barang = DB::table('barang')->where('slug',$request->slug)->get();
 		return view('dashboard.inventori.editObat',[
             'barang' => $barang,
-            "title" => "Edit Obat"
+            "title" => "Edit Obat",
+            'kategori' => $kategori
         ]);
 	}
-	
 
+    
+    public function tambahKategori(Request $request)
+	{
+		$this->validate($request, rules:[
+			'nama' => 'required',
+			'image' => 'required|mimes:jpg,jpeg,png,bmp,gif,svg,jfif'
+
+		]);
+		$img = $request->file('image');
+        $ext = $img->getClientOriginalExtension();
+        $name =  $request->image->getClientOriginalName();
+        $path = public_path('kategori');
+        $img->move($path, $name);
+        $slug =  Str::slug($request->nama,"-");
+		DB::table('kategori')->insert([
+			'nama' => $request->nama,
+			'gambar' =>$name,
+            'slug' =>$slug
+		]);
+		return redirect('/admin/kategori');
+	
+	}
+
+    public function updateKategori(Request $request)
+	{
+
+	
+	$this->validate($request, rules:[
+			'nama' => 'required',
+			'image' => 'image|mimes:jpg,jpeg,png,bmp,gif,svg'
+	]);
+    $slug =  Str::slug($request->nama,"-");
+
+	if($request->hasFile('image')){
+		$img = $request->file('image');
+		$ext = $img->getClientOriginalExtension();
+		$name =  $request->image->getClientOriginalName();
+		$path = public_path('kategori');
+		$img->move($path, $name);
+		DB::table('kategori')->where('id',$request->id)->update([
+			'nama' => $request->nama,
+			'gambar' =>$name,
+            'slug'=> $slug
+		]);
+	}else{
+	DB::table('kategori')->where('id',$request->id)->update([
+        'nama' => $request->nama,
+        'slug'=> $slug
+		]);
+	}
+
+		return redirect('/admin/kategori');
+	}
+	public function hapusKategori($id)
+	{
+	
+		DB::table('kategori')->where('id',$id)->delete();
+        return redirect('/admin/kategori' ); 
+	}
+
+    public function updateBarang(Request $request)
+	{
+        date_default_timezone_set('Asia/Jakarta');   
+        $time = date("Y-m-d H:i:s", time()); 
+
+        $this->validate($request, rules:[
+			'nama' => 'required',
+			'jumlah' => 'required',
+			'minimal'=> 'required',
+            'maksimal'=> 'required',
+            'kadaluarsa'=> 'required',
+            'harga'=> 'required',
+            'deskripsi'=> 'required',
+            'kategori'=> 'required',
+
+		]);
+		$content = $request->input('deskripsi');
+		$slug =  Str::slug($request->nama,"-");
+        if($request->hasFile('image')){
+            $img = $request->file('image');
+            $ext = $img->getClientOriginalExtension();
+            $name =  $request->image->getClientOriginalName();
+            $path = public_path('uploads');
+            $img->move($path, $name);
+            DB::table('barang')->where('id',$request->id)->update([
+                'nama' => $request->nama,
+                'kategori' => $request->kategori,
+                'deskripsi' => $content,
+                'sisa' =>$request->jumlah,
+                'minimal' =>$request->minimal,
+                'maksimal' =>$request->maksimal,
+                'kadaluarsa' =>$request->kadaluarsa,
+                'image' =>$name,
+                'slug' =>$slug,
+                'harga' =>$request->harga,
+                'tanggal_edit'=> $time
+            ]);
+        }else{
+            DB::table('barang')->where('id',$request->id)->update([
+                'nama' => $request->nama,
+                'kategori' => $request->kategori,
+                'deskripsi' => $content,
+                'sisa' =>$request->jumlah,
+                'minimal' =>$request->minimal,
+                'maksimal' =>$request->maksimal,
+                'kadaluarsa' =>$request->kadaluarsa,
+                'slug' =>$slug,
+                'harga' =>$request->harga,
+                'tanggal_edit'=> $time
+            ]);
+        }
+
+		return redirect('/admin/daftar-obat');
+	}
+    public function hapusBarang($id)
+	{
+	
+		DB::table('barang')->where('id',$id)->delete();
+        return redirect('/admin/daftar-obat' ); 
+	}
+
+    public function search(Request $request)
+    {
+        if($request->ajax()){
+                $a = 1;
+                $output="";
+                $products=DB::table('barang')
+                ->where('nama','LIKE','%'.$request->search."%")
+                ->orwhere('kategori','LIKE','%'.$request->search."%")
+                ->get();
+            
+                if ($products->isEmpty()){
+                    $products=DB::table('barang') ->get();
+                }
+                if($products){
+                    foreach ($products as $key => $product) {
+            
+                    $output.='<tr>'.
+                    '<td>'.$a.'</td>'.
+                    '<td>'.$product->id.'</td>'.
+                    '<td>'.$product->nama.'</td>'.
+                    '<td>'.$product->kategori.'</td>'.
+                    '<td>'.$product->sisa.'</td>'.
+                    '<td>
+                        <a href="/admin/edit-barang/'. $product->slug.'" class="btn btn-primary btn-sm">
+                            <i class="fa-solid fa-pen text-light"></i>
+                        </a>
+                        &emsp13;
+                        <a class="btn btn-danger btn-sm" href="/admin/daftar-obat/hapus-barang/.'.$product->id.'"
+                            onclick="return confirm(`Apakah anda yakin ingin menghapus?`)">
+                            <i class="fa-solid fa-trash-can text-light"></i>
+                        </a>
+                        &emsp;
+                        <a href="/admin/detail-obat/'.$product->slug.'"
+                            style="color: #1D242E; text-decoration: none;">Detail >></a>
+                    </td>'.
+                    '</tr>';
+                    $a++;
+            }
+            return Response($output);
+            }
+        }
+    }
+    
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 class UserController extends Controller
 {
     public function index (){
@@ -14,8 +15,13 @@ class UserController extends Controller
     }
 
     public function cekstok () {
+        $barang = DB::table('barang')
+		->orderby('id' , 'desc')
+		->paginate(9)->onEachSide(0);
+
         return view('user.cekStok', [
-            "title" => "Cek Stok Obat"
+            "title" => "Cek Stok Obat",
+            "barang" => $barang
         ]);
     }
 
@@ -25,15 +31,51 @@ class UserController extends Controller
         ]);
     }
 
-    public function kategori () {
+    public function kategori_barang() {
+        $kategori = DB::table('kategori')->get();
         return view('user.kategori', [
-            "title" => "Kategori"
+            "title" => "Kategori",
+            "kategori"=> $kategori
+        ]);
+    } 
+
+    public function detail (Request $request) {
+        $barang = DB::table('barang')->where('slug',$request->slug)->get();
+        return view('user.detail', [
+            "title" => "Detail",
+            "barang"=>$barang
+        ]);
+    }
+    public function listKategori (Request $request) {
+        $string1 = str_replace('-', ' ', $request->slug);
+        $string = strtolower($string1);
+        $barang = DB::table('barang')->where(strtolower('kategori'),$string)->paginate(9)->onEachSide(0);;
+        $title =  ucwords($string1);
+        return view('user.listKategori', [
+            "title" => "Detail Kategori $title",
+            "barang"=>$barang,
+            "namaKategori" => $title
         ]);
     }
 
-    public function detail () {
-        return view('user.detail', [
-            "title" => "Detail"
-        ]);
-    }
+    public function cari(Request $request)
+	{
+	
+		$cari = $request->cari;
+
+		$barang = DB::table('barang')
+		->where('nama','like',"%".$cari."%")
+		->orwhere('kategori','like',"%".$cari."%")
+		->orwhere('deskripsi','like',"%".$cari."%")
+		->orderby('tanggal_edit' , 'desc')
+		
+		->paginate(10)->withQueryString()->onEachSide(0);
+
+		return view('user.cekStok',[
+        'barang' => $barang,
+        "title" => "Cek Stok Obat",
+        "query" => $cari
+    ]);
+
+	}
 }
