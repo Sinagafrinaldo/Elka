e0671f39e50f62c42f8a2214002948ecf36b1317
Frinaldo Sinaga <84637791+Sinagafrinaldo@users.noreply.github.com>
2022-10-30 21:34:23 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-
+ 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,9 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [UserController::class, 'index']);
 Route::get('/cek-stok', [UserController::class, 'cekstok']);
 Route::get('/tentang-kami', [UserController::class, 'tentangkami']);
-Route::get('/kategori', [UserController::class, 'kategori']);
-Route::get('/detail', [UserController::class, 'detail']); 
-
+Route::get('/kategori-barang', [UserController::class, 'kategori_barang']);
+Route::get('/cari', [UserController::class, 'cari']);
+Route::get('/list-kategori/{slug}', [UserController::class, 'listKategori']);
+Route::get('/detail/{slug}', [UserController::class, 'detail']); 
+ 
 Route::prefix('admin')->group(function(){
     Route::get('/',[App\Http\Controllers\Admin\Auth\LoginController::class,'loginForm']);
     Route::get('/login',[App\Http\Controllers\Admin\Auth\LoginController::class,'loginForm'])->name('admin.login');
@@ -39,12 +41,20 @@ Route::prefix('admin')->group(function(){
     Route::get('/kategori',[App\Http\Controllers\Admin\HomeController::class,'kategori'])->name('admin.kategori');
     
     Route::post('/input-obat/store',[App\Http\Controllers\Admin\HomeController::class,'store']);
+    Route::post('/input-kategori/store',[App\Http\Controllers\Admin\HomeController::class,'tambahKategori']);
 
+    Route::post('/update-kategori',[App\Http\Controllers\Admin\HomeController::class,'updateKategori'])->name('admin.updateKategori');
+    Route::post('/update-barang',[App\Http\Controllers\Admin\HomeController::class,'updateBarang'])->name('admin.updateBarang');
+    
+    Route::get('/detail-obat/{slug}',[App\Http\Controllers\Admin\HomeController::class,'detail'])->name('admin.detail');
+    Route::get('/edit-barang/{slug}',[App\Http\Controllers\Admin\HomeController::class,'edit'])->name('admin.edit');
 
- Route::get('/detail-obat/{slug}',[App\Http\Controllers\Admin\HomeController::class,'detail'])->name('admin.detail');
- Route::get('/edit-barang/{slug}',[App\Http\Controllers\Admin\HomeController::class,'edit'])->name('admin.edit');
+    Route::get('/tambah-kategori',[App\Http\Controllers\Admin\HomeController::class,'inputKategori'])->name('admin.inputKategori');
+    Route::get('/edit-kategori/{slug}',[App\Http\Controllers\Admin\HomeController::class,'editKategori']);
 
- Route::get('/tambah-kategori',[App\Http\Controllers\Admin\HomeController::class,'inputKategori'])->name('admin.inputKategori');
- Route::get('/edit-kategori',[App\Http\Controllers\Admin\HomeController::class,'editKategori'])->name('admin.editKategori');
- 
+    Route::get('/daftar-obat/cari',[App\Http\Controllers\Admin\HomeController::class,'search']);
+
+    
+    Route::get('/kategori/hapus-kategori/{id}',[App\Http\Controllers\Admin\HomeController::class,'hapusKategori']);
+    Route::get('/daftar-obat/hapus-barang/{id}',[App\Http\Controllers\Admin\HomeController::class,'hapusBarang']);
 });
