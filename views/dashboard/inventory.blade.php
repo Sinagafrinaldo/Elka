98cfb9df907445890f6a014c3074c3ad061b6579
119140037 - Joy Ravelo Tarigan <56099222+joyrav09@users.noreply.github.com>
2022-11-02 11:25:12 +0700

    Delete views directory

--- views/dashboard/inventory.blade.php
+++
@@ -1,7 +0,0 @@
-<link rel="shortcut icon" href="/assets/images/icon.svg" type="image/x-icon">
-<title>Inventory || Elka Farma</title>
-@extends('layouts._dashboard.app')
-
-@section('content')
-   <h1> Inventory </h1>
-@endsection
