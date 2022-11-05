35dac4dd471d2de9bbcb23fb3a13e55ff2cdb9eb
Frinaldo Sinaga <84637791+Sinagafrinaldo@users.noreply.github.com>
2022-11-05 08:00:36 +0700

    fitur transaksi + laporan

--- resources/views/dashboard/laporanScreen/barangsisa.blade.php
+++ resources/views/dashboard/laporanScreen/barangsisa.blade.php
@@ -1,41 +1,70 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 @extends('dashboard.laporan')
 
 @section('title')
-    <a href="" class="text-dark fw-bold fs-4 text-decoration-none">Barang Sisa</a>
+<a href="" class="text-dark fw-bold fs-4 text-decoration-none">Barang Sisa</a>
 @endsection
 
 @section('table')
-    {{-- Table --}}
-    <div class="row mt-3 mx-1 p-2 bg-white">
-        <table class="table table-hover table-responsive text-center" style="color: #656a6e">
-            <thead>
-                <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">ID Produk</th>
-                    <th scope="col">Produk</th>
-                    <th scope="col">Kategori</th>
-                    <th scope="col">Tanggal</th>
-                    <th scope="col">Jumlah</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>12533</td>
-                    <td>Paramex</td>
-                    <td>Obat Kepala</td>
-                    <td>01/10/2022</td>
-                    <td>3</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>12533</td>
-                    <td>Paramex</td>
-                    <td>Obat Kepala</td>
-                    <td>01/10/2022</td>
-                    <td>3</td>
-                </tr>
-            </tbody>
-        </table>
+{{-- Table --}}
+<?php $a = 1; ?>
+<div class="row mt-3 mx-1 p-2 bg-white">
+    <table class="table table-hover table-responsive text-center" style="color: #656a6e">
+        <thead>
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">ID Produk</th>
+                <th scope="col">Produk</th>
+                <th scope="col">Kategori</th>
+                <th scope="col">Stok</th>
+            </tr>
+        </thead>
+        <tbody id="output">
+            @foreach ($barang as $b)
+
+            <tr>
+                <th scope="row">{{ $a }}</th>
+                <td>{{$b->id}}</td>
+                <td>{{$b->nama}}</td>
+                <td>{{$b->kategori}}</td>
+                @if ($b->sisa < $b->minimal)
+                    <td style="color: #F0483E"><b>{{$b->sisa}}</b></td>
+                    @else
+                    <td>{{$b->sisa}}</td>
+                    @endif
+            </tr>
+
+
+
+            <?php $a++; ?>
+            @endforeach
+        </tbody>
+    </table>
+    <div class="d-flex">
+        <div style="background-color: F0483E; height: 30px; width: 30px; border-radius: 5px"></div>
+        <div style="margin-left: 10px;"> Stok Barang Minim</div>
     </div>
+</div>
+<div class="pagination justify-content-center">
+    {{ $barang->links() }}
+</div>
+
+<script type="text/javascript">
+
+
+    $('#tampilMinim').on('click', function () {
+        $value = $(this).val();
+        $.ajax({
+            type: 'get',
+            url: '{{URL::to("/admin/laporan-barang-sisa/minim")}}',
+            data: { 'search': $value },
+            success: function (data) {
+                $('#output').html(data);
+            }
+        });
+    })
+</script>
+<script type="text/javascript">
+    $.ajaxSetup({ headers: { 'csrftoken': '{{ csrf_token() }}' } });
+</script>
 @endsection
