4088b1305dc533f31d93b24083b9e9da4ab01db4
mriskiaziz <mriski.119140160@student.itera.ac.id>
2022-11-12 23:04:55 +0700

    front end terbaru

--- resources/views/user/kategori.blade.php
+++ resources/views/user/kategori.blade.php
@@ -4,9 +4,19 @@
     <div class="container my-5 ">
         <h4 class="d-flex justify-content-center fw-bold">KATEGORI</h4>
         <hr>
-        <div class="row p-3">
+        <div class="row p-3 px-lg-5 ">
             @foreach ($kategori as $k)
-                <div class="col-6 col-lg p-2 d-flex justify-content-center">
+                <div class="col-6 col-lg-4 p-2 d-flex justify-content-center">
+                    <div class="card border py-lg-4  border-dark w-100">
+                        <div onClick="location.href='/list-kategori/{{ $k->slug }}'" style="cursor:pointer;"
+                            class="card-body d-flex flex-column flex-sm-row justify-content-center align-items-center">
+                            <img src="/kategori/{{ $k->gambar }}" class="me-sm-3" width="60" height="60">
+                            <h5 class="card-title pt-3 pt-sm-0" style="text-align: center;">{{ $k->nama }}</h5>
+                        </div>
+                    </div>
+                </div>
+
+                {{-- <div class="col-6 col-lg p-2 d-flex justify-content-center">
                     <div class="card" style="width: 18rem;">
                         <div onClick="location.href='/list-kategori/{{ $k->slug }}'" style="cursor:pointer;"
                             class="card-body bg-dark d-flex justify-content-center">
@@ -16,19 +26,19 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> --}}
             @endforeach
         </div>
     </div>
     <style>
-        h5 {
-            width: 150px;
-        }
+        /* h5 {
+                                                                                    width: 150px;
+                                                                                }
 
-        @media(max-width: 400px) {
-            h5 {
-                font-size: 1rem;
-            }
-        }
+                                                                                @media(max-width: 400px) {
+                                                                                    h5 {
+                                                                                        font-size: 1rem;
+                                                                                    }
+                                                                                } */
     </style>
 @endsection
